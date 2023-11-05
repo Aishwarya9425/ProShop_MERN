d58669739c684470d98981ef15fb2498d522abae
Aishwarya <aishwarya9425@gmail.com>
2023-11-05 15:22:33 +0530

    api slices, login

---
+++ frontend/src/slices/productsApiSlice.js
@@ -0,0 +1,90 @@
+import { PRODUCTS_URL } from '../constants';
+import { apiSlice } from './apiSlice';
+
+//we dont have to use fetch/axios or create async thunk
+//using rtk query 
+export const productsApiSlice = apiSlice.injectEndpoints({
+
+  endpoints: (builder) => ({
+
+    //get all products
+    getProducts: builder.query({
+      query: ({ keyword, pageNumber }) => ({
+        url: PRODUCTS_URL, //from constants
+        params: { keyword, pageNumber },
+      }),
+      keepUnusedDataFor: 5, // in seconds
+      providesTags: ['Products'],
+    }),
+
+    //get single product by id
+    getProductDetails: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+
+    createProduct: builder.mutation({
+      query: () => ({
+        url: `${PRODUCTS_URL}`,
+        method: 'POST',
+      }),
+      invalidatesTags: ['Product'],
+    }),
+
+
+    updateProduct: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}`,
+        method: 'PUT',
+        body: data,
+      }),
+      invalidatesTags: ['Products'],
+    }),
+
+
+    uploadProductImage: builder.mutation({
+      query: (data) => ({
+        url: `/api/upload`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
+
+    deleteProduct: builder.mutation({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+        method: 'DELETE',
+      }),
+      providesTags: ['Product'],
+    }),
+
+    createReview: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['Product'],
+    }),
+
+    getTopProducts: builder.query({
+      query: () => `${PRODUCTS_URL}/top`,
+      keepUnusedDataFor: 5,
+    }),
+  }),
+
+});
+
+//export all queries 
+export const {
+  useGetProductsQuery,
+  useGetProductDetailsQuery,
+  useCreateProductMutation,
+  useUpdateProductMutation,
+  useUploadProductImageMutation,
+  useDeleteProductMutation,
+  useCreateReviewMutation,
+  useGetTopProductsQuery,
+} = productsApiSlice;
